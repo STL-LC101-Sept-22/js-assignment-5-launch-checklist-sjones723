626a66c797ab5bb64e528e62d17a58b3b39619e2
Sydney Jones <sydneyjones87@gmail.com>
2022-12-04 17:24:00 -0600

    form validation nulls

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+  let form = document.querySelector("form");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+  let pilotNameInput = document.querySelector("input[name=pilotName]");
+  let copilotNameInput = document.querySelector("input[name=copilotName]");
+  let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+  let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+  let listedPlanets;
+  // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+  let listedPlanetsResponse = myFetch();
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
+    })
+    .then(function () {
+      console.log(listedPlanets);
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    });
+
+  form.addEventListener("submit", function (event) {
+    if (
+      pilotNameInput.value === "" ||
+      copilotNameInput.value === "" ||
+      fuelLevelInput.value === "" ||
+      cargoMassInput.value === ""
+    ) {
+      alert("All fields are required!");
+      // stop the form submission
+      event.preventDefault();
+    }
+  });
 });
